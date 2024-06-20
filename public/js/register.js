35024b0d8a318ebe03166d6622fc34592f940550
Pranay Kumar <151812978+pranaykumar2@users.noreply.github.com>
2024-06-20 11:19:57 +0530

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,121 +1,132 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
-  const captureBtn = document.getElementById('capture-btn');
-  const registerForm = document.getElementById('register-form');
-  const imageInput = document.getElementById('image');
-  const videoPopup = document.getElementById('video-popup');
-  const openVideoBtn = document.getElementById('open-video-btn');
-  const closePopupBtn = document.getElementById('close-popup');
-  const capturedImage = document.getElementById('captured-image');
-  const notification = document.getElementById('notification'); // Get the notification element
+  window.onload = function() {
+    setTimeout(function() {
+      document.getElementById('popup').classList.add('slideIn');
+    }, 1000);
 
-  let stream;
+    const letsGoButton = document.querySelector('.button button');
+    letsGoButton.addEventListener('click', function() {
+      document.querySelector('.flip-container').classList.toggle('flipped');
+      document.querySelector('.back').style.display = 'flex';
+    });
 
-  // Open camera when the button is clicked
-  openVideoBtn.addEventListener('click', () => {
-    videoPopup.style.display = 'flex';
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(s => {
-        stream = s;
-        video.srcObject = stream;
-        video.style.display = 'block';
-        capturedImage.style.display = 'none';
-      })
-      .catch(err => {
-        console.error("Error accessing the camera: " + err);
-      });
-  });
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const captureBtn = document.getElementById('capture-btn');
+    const registerForm = document.getElementById('register-form');
+    const imageInput = document.getElementById('image');
+    const videoPopup = document.getElementById('video-popup');
+    const openVideoBtn = document.getElementById('open-video-btn');
+    const closePopupBtn = document.getElementById('save-btn');
+    const capturedImage = document.getElementById('captured-image');
+    const notification = document.getElementById('notification');
 
-  // Close the video popup when clicking outside of the popup content
-  videoPopup.addEventListener('click', (e) => {
-    if (e.target === videoPopup) {
-      stopVideoStream();
-      videoPopup.style.display = 'none';
-    }
-  });
+    let stream;
 
-  // Close the video popup when clicking the close button
-  closePopupBtn.addEventListener('click', () => {
-    stopVideoStream();
-    videoPopup.style.display = 'none';
-  });
+    openVideoBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      videoPopup.style.display = 'flex';
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(s => {
+          stream = s;
+          video.srcObject = stream;
+          video.style.display = 'block';
+          capturedImage.style.display = 'none';
+        })
+        .catch(err => {
+          console.error("Error accessing the camera: " + err);
+        });
+    });
 
-  // Capture or recapture the image
-  captureBtn.addEventListener('click', () => {
-    if (captureBtn.textContent === 'Capture') {
-      captureImage();
-    } else if (captureBtn.textContent === 'Re-capture') {
-      enableVideoStream();
-    }
-  });
-
-  registerForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData(registerForm);
-    try {
-      const response = await fetch('/register', {
-        method: 'POST',
-        body: formData
-      });
-      if (response.ok) {
-        showNotification('Image uploaded successfully'); // Call showNotification on success
-      } else {
-        throw new Error('Failed to upload image');
+    videoPopup.addEventListener('click', (e) => {
+      if (e.target === videoPopup || e.target === closePopupBtn) {
+        stopVideoStream();
+        videoPopup.style.display = 'none';
       }
-    } catch (err) {
-      console.error(err);
-      showNotification('Failed to upload image');
-    }
-  });
+    });
 
-  // Capture the image from the video stream
-  function captureImage() {
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const dataUrl = canvas.toDataURL('image/png');
-    imageInput.value = dataUrl;
+    captureBtn.addEventListener('click', () => {
+      if (captureBtn.textContent === 'Capture') {
+        captureImage();
+      } else if (captureBtn.textContent === 'Re-capture') {
+        enableVideoStream();
+      }
+    });
 
-    // Display the captured image
-    capturedImage.src = dataUrl;
-    capturedImage.style.display = 'block';
-    video.style.display = 'none';
+    registerForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      const formData = new FormData(registerForm);
+      formData.append('image', imageInput.value);
 
-    stopVideoStream();
-    captureBtn.textContent = 'Re-capture';
-  }
+      try {
+        const response = await fetch('/register', {
+          method: 'POST',
+          body: formData
+        });
+        if (response.ok) {
+          showNotification('Image uploaded successfully');
+        } else {
+          throw new Error('Failed to upload image');
+        }
+      } catch (err) {
+        console.error(err);
+        showNotification('Failed to upload image');
+      }
+    });
 
-  // Enable video stream for re-capture
-  function enableVideoStream() {
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(s => {
-        stream = s;
-        video.srcObject = stream;
-        captureBtn.textContent = 'Capture';
+    function captureImage() {
+      const context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const dataUrl = canvas.toDataURL('image/png');
+      imageInput.value = dataUrl;
 
-        // Hide the captured image
-        capturedImage.style.display = 'none';
-        video.style.display = 'block';
-      })
-      .catch(err => {
-        console.error("Error accessing the camera: " + err);
+      const imagePreviewSection = document.getElementById('image-preview-section');
+      imagePreviewSection.innerHTML = `<a href="#" id="show-image-link" target="_blank">Show Image</a><img id="preview-image" src="${dataUrl}" alt="Captured Image Preview" style="max-width: 100%; display: none;">`;
+      imagePreviewSection.style.display = 'block';
+
+      const showImageLink = document.getElementById('show-image-link');
+      const previewImage = document.getElementById('preview-image');
+      showImageLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        previewImage.style.display = previewImage.style.display === 'none' ? 'block' : 'none';
       });
-  }
 
-  // Stop the video stream
-  function stopVideoStream() {
-    if (stream) {
-      stream.getTracks().forEach(track => track.stop());
-      video.srcObject = null;
+      capturedImage.src = dataUrl;
+      capturedImage.style.display = 'block';
+      video.style.display = 'none';
+
+      stopVideoStream();
+      captureBtn.textContent = 'Re-capture';
     }
-  }
 
-  // Function to show notification
-  function showNotification(message) {
-    notification.innerText = message;
-    notification.classList.add('show');
-    setTimeout(() => {
-      notification.classList.remove('show');
-    }, 3000);
+    function enableVideoStream() {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(s => {
+          stream = s;
+          video.srcObject = stream;
+          captureBtn.textContent = 'Capture';
+
+          capturedImage.style.display = 'none';
+          video.style.display = 'block';
+        })
+        .catch(err => {
+          console.error("Error accessing the camera: " + err);
+        });
+    }
+
+    function stopVideoStream() {
+      if (stream) {
+        stream.getTracks().forEach(track => track.stop());
+        video.srcObject = null;
+      }
+    }
+
+    function showNotification(message) {
+      notification.innerText = message;
+      notification.classList.add('show');
+      setTimeout(() => {
+        notification.classList.remove('show');
+      }, 3000);
+    }
   }
 });
